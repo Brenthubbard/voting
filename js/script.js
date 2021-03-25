fef7093a6e35db839389750d0c6691e5e03527ba
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-03-25 12:45:02 -0600

    complete page
    Co-authored-by: John Edmondson <edmondsonj@gmail.com>
    Co-authored-by: Ahmed Ghouzlane <ahmedghouzlane@gmail.com>

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@ $(document).ready(function () {
     e.preventDefault();
     const age = $("#fage").val();
 
-    if (age > 17 ) {
-      $("#voter").show();
-    } else if ( age < 18 && age > 125) {
+    if (age < 18) {
       $("#nonVoter").show();
+    } else if (age > 150) {
+      alert("You entered something other than a number. Please enter a number!");
+    } else if (age >= 18) {
+      $("#voter").show();
     } else {
       alert("You entered something other than a number. Please enter a number!");
     }
-
   });
 
 });
